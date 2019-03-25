74e3c422afd63f998c39fe992e670d81f8a306c5
Singee <imsingee@gmail.com>
2019-03-26 00:26:02 +0800

    C2-5

---
+++ C2-5/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+    cin >> a >> b >> op;
+
+    switch (op) {
+        case '+':
+            cout << a + b << endl;
+            break;
+        case '-':
+            cout << a - b << endl;
+            break;
+        case '*':
+            cout << a * b << endl;
+            break;
+        case '/':
+            cout << a / b << endl;
+            break;
+        case '%':
+            cout << a % b << endl;
+            break;
+    }
+
+    return 0;
+}
